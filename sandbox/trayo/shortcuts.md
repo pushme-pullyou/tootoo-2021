daed19f36dd5f36af210a10e28817233ca520227
Theo Armour <t.armour@gmail.com>
2021-07-19 14:18:26 -0700

    update readme

---
+++ sandbox/trayo/shortcuts.md
@@ -0,0 +1,55 @@
+
+
+win10:<br>
+* <a href="https://support.microsoft.com/en-us/help/12445/windows-keyboard-shortcuts"
+	target="_blank">Keyboard shortcuts in Windows</a><br>
+* <a href="https://support.microsoft.com/en-us/help/4042244/windows-10-use-dictation"
+	target="_blank">dictation</a><br>
+* darkmode: settings > personalization<br>
+</p>
+
+<p>
+gMail:<br>
+* keyboard shortcuts: ?<br>
+* space bar: scroll preview<br>
+* cursor keys: scroll messages
+<p>
+<a href="https://support.google.com/chrome/answer/157179?hl=en" target="_blank">chrome browser</a><br>
+* help: F1<br>
+* close tab: ctrl-w<br>
+Jump to the next open tab: Ctrl + Tab or Ctrl + PgDn<br>
+* caret browsing: F7<br>
+* Open a new window in Incognito mode: Ctrl + Shift + n
+</p>
+<p>
+chromebook: <a href="https://blog.google/products/chromebooks/" target="_blank">blog</a><br>
+</b>* help: ctrl-alt-/</b><br>
+* <a href="https://support.google.com/chromebook/answer/177893?hl=en" target="_blank">accessibility
+	features - dictation</a>
+</p>
+<p>
+vs code:<br>
+* >> <a href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf"
+	target="_blank">keyboard shortcuts</a>: Ctrl + K Ctrl + R<br>
+* Curser add select next: Ctrl + D<br>
+* Cursor add below: alt-ctrl-&darr;<br>
+* Definition: F12<br>
+* Delete prev||next: ctrl + bspace||delete<br>
+* Folding: ctrl-K-ctrl-1 || ctrl+shift+[||]<br>
+* Search case|regex|whole word: Alt+C|R|W T <br>
+* Selection expand: alt + shift + rArr<br>
+* Settings json: ctrl + ,<br>
+* Terminal: ctrl + `<br>
+* Word wrap: alt + z<br>
+Custom<br>
+* Title Case: ctrl + i</br>
+* home||end: alt + lArr||rArr<br>
+
+</p>
+<p>
+<a href="https://docs.emmet.io/cheat-sheet/" target="_blank">emmet cheat-sheet</a><br>
+* html:5<br>
+* lorem100<br>
+* ul>li.item$*5 << type this then press tab<br>
+	* <br>
+</p>
---
+++ sandbox/trayo/v-2021-07-19/gtb-github-text-box.js
@@ -0,0 +1,162 @@
+// copyright 2021 Theo Armour. MIT license.
+//* global  */
+// jshint esversion: 6
+// jshint loopfunc: true
+
+function GTB() {
+
+	this.user = "pushme-pullyou";
+	this.repo = "tootoo-2021";
+	this.branch = "master";
+	this.file = "test-cases/text-to-hack.html";
+	this.idDivTextBox = "GTBdivTextBox1";
+	this.idMessage = "GTBdivMessage";
+	this.idContent = "GTBdivContent";
+	this.idButPut = "GTBbutPut";
+
+
+	this.init = ( params = {} ) => {
+		//console.log( "params", params );
+
+		this.accessToken = localStorage.getItem( 'githubAccessToken' ) || "";
+
+		this.user = params.user || this.user;
+		this.repo = params.repo || this.repo;
+		this.branch = params.branch || this.branch;
+		this.file = params.file || this.file;
+		this.idDivTextBox = params.idDivTextBox || this.idDivTextBox;
+		this.idMessage = params.idMessage || this.idMessage;
+		this.idContent = params.idContent || this.idContent;
+		this.idButPut = params.idButPut || this.idButPut;
+
+		this.url = `https://api.github.com/repos/${ this.user }/${ this.repo }/contents/${ this.file }`;
+
+		this.divTextBox = window[ this.idDivTextBox ];
+
+		const htm = `
+<div>
+	<button id=${ this.idButPut } title="Press Alt-S">putToGitHub</button>
+	<div id=${ this.idMessage } style=display:inline; ></div>
+</div>
+<div id=${ this.idContent } style="border: 1px #888 solid;overflow:auto;padding: 0.5em;resize:both;" contentEditable ></div>
+	`;
+
+		this.divTextBox.innerHTML = htm;
+
+		this.divContent = window[ this.idContent ];
+		//console.log( "this.divContent ", this.divContent );
+
+		this.divMessage = window[ this.idMessage ];
+
+		window[ this.idButPut ].onclick = () => this.putFileToGitHub( this.url );
+
+		this.requestFile();
+
+		window.addEventListener( "beforeunload", this.checkForChange, false );
+
+	};
+
+
+
+	this.checkForChange = function ( event ) {
+
+		console.log( "event", this.divContent.innerHTML, this.contentHtml );
+
+		if ( this.divContent.innerHTML === this.contentHtml ) {
+			console.log( "equal" );
+			return;
+		} else {
+
+			console.log( "not equal" );
+		}
+
+		event.preventDefault();
+
+		event.returnValue = "Are you sure you want to exit?";
+
+	};
+
+
+
+	this.requestFile = function ( event ) {
+
+		const xhr = new XMLHttpRequest();
+		xhr.open( "GET", this.url, true );
+		xhr.setRequestHeader( "Authorization", "token " + this.accessToken );
+		xhr.responseType = "json";
+		xhr.onerror = ( xhr ) => console.log( "error:", xhr );
+		//xhr.onprogress = ( xhr ) => console.log( "bytes loaded:", xhr.loaded );
+		xhr.onload = ( xhr ) => this.onLoad( xhr );
+		xhr.send( null );
+
+	};
+
+
+
+	this.onLoad = function ( xhr ) {
+
+		console.log( "xhr.target.response", xhr.target.response);
+		this.sha = xhr.target.response.sha;
+		this.content = xhr.target.response.content;
+		this.htm = atob( this.content );
+		this.divContent.innerHTML = this.htm;
+		//console.log( "htm", this.htm );
+		this.divMessage.innerText = `Get:${ new Date().toLocaleString() } bytes:${ this.content.length }`;
+		// sha: ${ this.sha; }`;
+
+	};
+
+
+	/////////
+
+
+	this.putFileToGitHub = function ( url ) {
+
+		if ( url === "" ) { alert( "No URL" ); return; }
+		//console.log( "this.url", url );
+
+		const xhr = new XMLHttpRequest();
+		xhr.open( "GET", url, true );
+		xhr.setRequestHeader( "Authorization", "token " + this.accessToken );
+		xhr.responseType = "json";
+		xhr.onerror = ( xhr ) => console.log( "error:", xhr );
+		//xhr.onprogress = ( xhr ) => console.log( "bytes loaded:", xhr.loaded );
+		xhr.onload = ( xhr ) => {
+			//console.log( "xhr", xhr );
+			this.sha = xhr.target.response.sha;
+			this.putFile();
+		};
+		xhr.send( null );
+
+	};
+
+
+
+	this.putFile = function () {
+
+		this.content = this.divContent.innerHTML;
+
+		const codedData = window.btoa( this.content ); // encode the string
+
+		const body = JSON.stringify( {
+			"branch": this.branch,
+			"content": codedData,
+			"message": `add to file`,
+			"sha": this.sha
+
+		} );
+
+		const xhr = new XMLHttpRequest();
+		xhr.open( "PUT", this.url, true );
+		xhr.setRequestHeader( "Authorization", "token " + this.accessToken );
+		xhr.setRequestHeader( "Content-Type", "application/json" );
+		xhr.onerror = ( xhr ) => console.log( "error:", xhr );
+		xhr.onprogress = ( xhr ) => console.log( "bytes loaded:", xhr.loaded );
+		xhr.send( body );
+
+		this.divMessage.innerText = `Put: ${ new Date().toLocaleString() } bytes:${ this.content.length }`;
+		//+ `sha: ${ this.sha; }`;
+
+	};
+
+}
