7e16f81902fefc46689e00def4fe8e2b2549e4d2
Theo Armour <t.armour@gmail.com>
2021-10-03 21:12:07 -0700

    update readme

--- apps/notesy-m/r-2021-10-03/ckm-ckeditor-md.js
+++ apps/notesy-m/r-2021-10-03/ckm-ckeditor-md.js
@@ -3,28 +3,29 @@
 // jshint esversion: 6
 // jshint loopfunc: true
 
-const CKH = {};
+const CKM = {};
 
 
-CKH.init = function ( url ) {
+CKM.init = function ( url) {
 
-	CKH.parentContent = divMainContent;
-	CKH.defaultFile = url || "snippets/notes.htm";
+	CKM.parentContent = divMainContent;
+	CKM.defaultFile = url || "README.md";
 
-	CKH.base = "https://api.github.com/repos/theo-armour/qdata/contents/";
-	//CKH.base = `https://api.github.com/repos/${ COR.user }/${ COR.repo }/contents/`;
+	//CKM.base = "https://api.github.com/repos/pushme-pullyou/tootoo-2021/contents/";
+	CKM.base = "https://api.github.com/repos/theo-armour/qdata/contents/";
+	//CKM.base = `https://api.github.com/repos/${ COR.user }/${ COR.repo }/contents/`;
 
-	CKH.accessToken = localStorage.getItem( "githubAccessToken" ) || "";
+	CKM.accessToken = localStorage.getItem( "githubAccessToken" ) || "";
 
-	if ( CKH.accessToken === "null" || CKH.accessToken === "" ) {
+	if ( CKM.accessToken === "null" || CKM.accessToken === "" ) {
 
-		CKH.accessToken = prompt( "Enter GitHub Personal Access Token" )
+		CKM.accessToken = prompt( "Enter GitHub Personal Access Token" );
 
-		localStorage.setItem( "githubAccessToken", CKH.accessToken );
+		localStorage.setItem( "githubAccessToken", CKM.accessToken );
 
 	}
 
-	CKH.parentContent.innerHTML = `
+	CKM.parentContent.innerHTML = `
 
 	<div id="container"  >
 
@@ -34,25 +35,25 @@ CKH.init = function ( url ) {
 
 	<div id="wordCount"></div>`;
 
-	window.addEventListener( "hashchange", CKH.onHashChange, false );
+	window.addEventListener( "hashchange", CKM.onHashChange, false );
 
-	window.addEventListener( "beforeunload", CKH.checkForChange );
+	window.addEventListener( "beforeunload", CKM.checkForChange );
 
 	const scr = document.body.appendChild( document.createElement( "script" ) );
-	scr.onload = () => CKH.createEditor();
-	scr.src = "https://pushme-pullyou.github.io/tootoo-2021/lib10/ckeditor5/build/ckeditor.js";
+	scr.onload = () => CKM.createEditor();
+	scr.src = "https://pushme-pullyou.github.io/tootoo-2021/lib10/ckeditor5-markdown/build/ckeditor.js";
 
 };
 
 
 
-CKH.onHashChange = function () {
+CKM.onHashChange = function () {
 
-	if ( CKH.contentEditor !== undefined ) {
+	if ( CKM.contentEditor !== undefined ) {
 
-	// 	console.log( "equal", CKH.editor.data.get() === CKH.contentEditor );
+	  //console.log( "equal", CKM.editor.data.get() === CKM.contentEditor );
 
-		if ( CKH.editor.data.get() !== CKH.contentEditor ) {
+		if ( CKM.editor.data.get() !== CKM.contentEditor ) {
 
 			const response = confirm( "Changes you made may not be saved. Click OK to proceed without saving" );
 
@@ -62,42 +63,42 @@ CKH.onHashChange = function () {
 
 	}
 
-	CKH.hash = location.hash ? location.hash.slice( 1 ) : CKH.defaultFile;
-	console.log( "file", CKH.hash.split( "/" ).pop() );
+	CKM.hash = location.hash ? location.hash.slice( 1 ) : CKM.defaultFile;
+	console.log( "file", CKM.hash.split( "/" ).pop() );
 
-	CKH.url = CKH.base + CKH.hash;
+	CKM.url = CKM.base + CKM.hash;
 
-	CKH.requestFile();
+	CKM.requestFile();
 
 };
 
 
 
-CKH.requestFile = function () {
-	//console.log( "CKH.hash ", CKH.hash );
+CKM.requestFile = function () {
+	//console.log( "CKM.hash ", CKM.hash );
 
 	const xhr = new XMLHttpRequest();
-	xhr.open( "GET", CKH.url, true );
-	xhr.setRequestHeader( "Authorization", "token " + CKH.accessToken );
+	xhr.open( "GET", CKM.url, true );
+	xhr.setRequestHeader( "Authorization", "token " + CKM.accessToken );
 	xhr.responseType = "json";
 	xhr.onerror = ( xhr ) => console.log( "error:", xhr );
 	//xhr.onprogress = ( xhr ) => console.log( "bytes loaded:", xhr.loaded );
-	xhr.onload = CKH.onLoad;
+	xhr.onload = CKM.onLoad;
 	xhr.send( null );
 
 };
 
 
 
-CKH.onLoad = function ( xhr ) {
+CKM.onLoad = function ( xhr ) {
 
-	CKH.content = atob( xhr.target.response.content );
+	CKM.content = atob( xhr.target.response.content );
 
-	CKH.editor.data.set( CKH.content );
+	CKM.editor.data.set( CKM.content );
 
-	CKH.contentEditor = CKH.editor.data.get();
+	CKM.contentEditor = CKM.editor.data.get();
 
-	spnMessage.innerText = `Get ${ new Date().toLocaleString().split( "," ).pop().slice( 1, -3 ) } ${ CKH.contentEditor.length }`;
+	spnMessage.innerText = `Get ${ new Date().toLocaleString().split( "," ).pop().slice( 1, -3 ) } ${ CKM.contentEditor.length }`;
 
 };
 
@@ -105,19 +106,20 @@ CKH.onLoad = function ( xhr ) {
 
 //////////
 
-CKH.getSha = function () {
+CKM.getSha = function () {
 
-	//if ( CKH.url === "" ) { alert( "No URL" ); return; }
+	if ( CKM.url === "" ) { alert( "No URL" ); return; }
 
 	const xhr = new XMLHttpRequest();
-	xhr.open( "GET", CKH.url, true );
-	xhr.setRequestHeader( "Authorization", "token " + CKH.accessToken );
+	xhr.open( "GET", CKM.url, true );
+	xhr.setRequestHeader( "Authorization", "token " + CKM.accessToken );
 	xhr.responseType = "json";
 	xhr.onerror = ( xhr ) => console.log( "error:", xhr );
 	//xhr.onprogress = ( xhr ) => console.log( "bytes loaded:", xhr.loaded );
 	xhr.onload = ( xhr ) => {
-		CKH.sha = xhr.target.response.sha;
-		CKH.putFile();
+		//console.log( "", xhr );
+		CKM.sha = xhr.target.response.sha;
+		CKM.putFile();
 	};
 	xhr.send( null );
 
@@ -125,40 +127,40 @@ CKH.getSha = function () {
 
 
 
-CKH.putFile = function () {
+CKM.putFile = function () {
 
-	CKH.contentEditor = CKH.editor.data.get();
-	//console.log( "CKH.contentEditor.length", CKH.contentEditor.length );
+	CKM.contentEditor = CKM.editor.data.get();
+	//console.log( "CKM.contentEditor.length", CKM.contentEditor.length );
 
-	const codedData = window.btoa( CKH.contentEditor ); // encode the string
+	const codedData = window.btoa( CKM.contentEditor ); // encode the string
 
 	const body = JSON.stringify( {
-		"branch": CKH.branch,
+		"branch": CKM.branch,
 		"content": codedData,
 		"message": `add to file`,
-		"sha": CKH.sha
+		"sha": CKM.sha
 
 	} );
 
 	xhr = new XMLHttpRequest();
-	xhr.open( "PUT", CKH.url, true );
-	xhr.setRequestHeader( "Authorization", "token " + CKH.accessToken );
+	xhr.open( "PUT", CKM.url, true );
+	xhr.setRequestHeader( "Authorization", "token " + CKM.accessToken );
 	xhr.setRequestHeader( "Content-Type", "application/json" );
 	xhr.onerror = ( xhr ) => console.log( "error:", xhr );
 	//xhr.onprogress = ( xhr ) => console.log( "bytes loaded:", xhr.loaded );
 	xhr.send( body );
 
-	spnMessage.innerText = `Put ${ new Date().toLocaleString().split( "," ).pop().slice( 1, -3 ) } ${ CKH.contentEditor.length }`;
+	spnMessage.innerText = `Put ${ new Date().toLocaleString().split( "," ).pop().slice( 1, -3 ) } ${ CKM.contentEditor.length }`;
 
 };
 
 
 
-CKH.checkForChange = function ( event ) {
+CKM.checkForChange = function ( event ) {
 
-	if ( CKH.editor.data.get() === CKH.contentEditor ) { return; }
+	if ( CKM.editor.data.get() === CKM.contentEditor ) { return; }
 
-	console.log( "file", CKH.url.split( "/" ).pop() );
+	console.log( "file", CKM.url.split( "/" ).pop() );
 
 	event.preventDefault();
 
@@ -168,7 +170,7 @@ CKH.checkForChange = function ( event ) {
 
 
 
-CKH.onKeyUp = function ( event ) {
+CKM.onKeyUp = function ( event ) {
 
 	//console.log( "key", event.keyCode );
 
@@ -176,7 +178,7 @@ CKH.onKeyUp = function ( event ) {
 
 	if ( event.altKey && event.keyCode === 83 ) {
 
-		CKH.getSha();
+		CKM.getSha();
 
 	}
 
@@ -186,7 +188,7 @@ CKH.onKeyUp = function ( event ) {
 
 //////////
 
-CKH.createEditor = function ( content ) {
+CKM.createEditor = function ( content ) {
 
 	ClassicEditor
 		.create( document.querySelector( ".editor" ), {
@@ -244,9 +246,9 @@ CKH.createEditor = function ( content ) {
 
 		.then( editor => {
 
-			CKH.editor = editor; // create a global
+			CKM.editor = editor; // create a global
 
-			CKH.onHashChange();
+			CKM.onHashChange();
 
 		} )
 
